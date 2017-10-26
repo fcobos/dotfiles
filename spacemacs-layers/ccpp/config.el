ead33fc3ec09ad79da993911e6668946af07824d
Felix M. Cobos <felix.cobos@gmail.com>
2017-10-26 08:41:16 +0200

    Added gdb-mi. Formatting.

--- spacemacs-layers/ccpp/config.el
+++ spacemacs-layers/ccpp/config.el
@@ -1,24 +1,18 @@
 ;;enable clang-format on save for projects with a .clang-format file at its root
 (defun clang-format-buffer-smart ()
- "Reformat buffer if .clang-format exists in the projectile root."
- (when (f-exists? (expand-file-name ".clang-format" (projectile-project-root)))
-   (clang-format-buffer)))
+  "Reformat buffer if .clang-format exists in the projectile root."
+  (when (f-exists? (expand-file-name ".clang-format"
+                                     (projectile-project-root)))
+    (clang-format-buffer)))
 (defun clang-format-buffer-smart-on-save ()
- "Add auto-save hook for clang-format-buffer-smart."
- (add-hook 'before-save-hook 'clang-format-buffer-smart nil t))
+  "Add auto-save hook for clang-format-buffer-smart."
+  (add-hook 'before-save-hook 'clang-format-buffer-smart
+            nil t))
 (spacemacs/add-to-hooks 'clang-format-buffer-smart-on-save
-                       '(c-mode-hook c++-mode-hook))
+                        '(c-mode-hook c++-mode-hook))
 
 ;; disable syntactic-indentation
 (require 'cc-mode)
 (add-to-list 'c-mode-common-hook
-             (lambda () (setq c-syntactic-indentation nil)))
-
-
-;;configure gdb
-(setq
-;; use gdb-many-windows by default when `M-x gdb'
-gdb-many-windows t
-;; Non-nil means display source file containing the main routine at startup
-gdb-show-main t)
-
+             (lambda ()
+               (setq c-syntactic-indentation nil)))
--- spacemacs-layers/ccpp/packages.el
+++ spacemacs-layers/ccpp/packages.el
@@ -29,23 +29,10 @@
 
 ;;; Code:
 
-(defconst ccpp-packages
-  '(
-    cc-mode
-    irony
-    company-irony
-    flycheck
-    flycheck-irony
-    irony-eldoc
-    company-irony-c-headers
-    rtags
-    helm-rtags
-    clang-format
-    disaster
-    cmake-mode
-    semantic
-    srefactor
-    )
+(defconst ccpp-packages '(cc-mode irony company-irony flycheck flycheck-irony
+                                  irony-eldoc company-irony-c-headers rtags
+                                  helm-rtags clang-format disaster cmake-mode
+                                  semantic srefactor gdb-mi)
   "The list of Lisp packages required by the ccpp layer.
 
 Each entry is either:
@@ -76,164 +63,150 @@ Each entry is either:
 (defun ccpp/init-cc-mode ()
   (use-package cc-mode
     :defer t
-    :init
-    (progn
-      (add-to-list 'auto-mode-alist
-                   `("\\.h\\'" . c++-mode)))
-    :config
+    :init (progn
+            (add-to-list 'auto-mode-alist
+                         `("\\.h\\'" . c++-mode))):config
     (progn
       (require 'compile)
       (c-toggle-auto-newline 1)
-      (spacemacs/set-leader-keys-for-major-mode 'c-mode
-        "ga" 'projectile-find-other-file
-        "gA" 'projectile-find-other-file-other-window)
-      (spacemacs/set-leader-keys-for-major-mode 'c++-mode
-        "ga" 'projectile-find-other-file
+      (spacemacs/set-leader-keys-for-major-mode
+        'c-mode "ga" 'projectile-find-other-file "gA"
+        'projectile-find-other-file-other-window)
+      (spacemacs/set-leader-keys-for-major-mode
+        'c++-mode "ga" 'projectile-find-other-file
         "gA" 'projectile-find-other-file-other-window))))
 
 (defun ccpp/post-init-company ()
-      (global-company-mode t)
-      ;; setup company backends
-      (add-hook 'after-init-hook 'global-company-mode)
-      (setq company-backends (delete 'company-clang company-backends))
-      )
+  (global-company-mode t)
+  ;; setup company backends
+  (add-hook 'after-init-hook 'global-company-mode)
+  (setq company-backends (delete 'company-clang company-backends)))
 
 (defun ccpp/init-company-irony ()
   (use-package company-irony
     :defer t
-    :init
-    (progn
-    (eval-after-load 'company
-      '(add-to-list 'company-backends '(company-irony)))
-    )
-    )
-  )
+    :init (progn
+            (eval-after-load 'company
+              '(add-to-list 'company-backends
+                            '(company-irony))))))
 
 (defun ccpp/init-company-irony-c-headers ()
   (use-package company-irony-c-headers
     :defer t
-    :init
-    (progn
-    (eval-after-load 'company
-      '(add-to-list 'company-backends '(company-irony-c-headers)))
-    )
-    )
-  )
+    :init (progn
+            (eval-after-load 'company
+              '(add-to-list 'company-backends
+                            '(company-irony-c-headers))))))
 
 
 (defun ccpp/init-semantic ()
   (use-package semantic
     :defer t
-    :init
-    (progn
-      ;; setup semantic
-      ;; (add-to-list 'semantic-default-submodes 'global-semantic-stickyfunc-mode)
-      (setq srecode-map-save-file (concat spacemacs-cache-directory "srecode-map.el"))
-      (setq semanticdb-default-save-directory (concat spacemacs-cache-directory "semanticdb/"))
-      (setq semanticdb-find-default-throttle '(file local project))
-      (unless (file-exists-p semanticdb-default-save-directory)
-        (make-directory semanticdb-default-save-directory))
-      (spacemacs/add-to-hooks 'semantic-mode '(c-mode-hook c++-mode-hook))
-      (semantic-mode 1)
-      )
-    )
-  )
+    :init (progn
+            ;; setup semantic
+            ;; (add-to-list 'semantic-default-submodes 'global-semantic-stickyfunc-mode)
+            (setq srecode-map-save-file (concat spacemacs-cache-directory "srecode-map.el"))
+            (setq semanticdb-default-save-directory (concat spacemacs-cache-directory "semanticdb/"))
+            (setq semanticdb-find-default-throttle '(file local project))
+            (unless (file-exists-p semanticdb-default-save-directory)
+              (make-directory semanticdb-default-save-directory))
+            (spacemacs/add-to-hooks 'semantic-mode
+                                    '(c-mode-hook c++-mode-hook))
+            (semantic-mode 1))))
 
 (defun ccpp/init-srefactor ()
   (use-package srefactor
     :defer t
-    :init
+    :init (progn
+            (defun spacemacs/lazy-load-srefactor ()
+              "Lazy load the package."
+              (require 'srefactor)
+              ;; currently, evil-mode overrides key mapping of srefactor menu
+              ;; must expplicity enable evil-emacs-state. This is ok since
+              ;; srefactor supports j,k,/ and ? commands when Evil is
+              ;; available
+              (add-hook 'srefactor-ui-menu-mode-hook 'evil-emacs-state))):config
     (progn
-      (defun spacemacs/lazy-load-srefactor ()
-        "Lazy load the package."
-        (require 'srefactor)
-        ;; currently, evil-mode overrides key mapping of srefactor menu
-        ;; must expplicity enable evil-emacs-state. This is ok since
-        ;; srefactor supports j,k,/ and ? commands when Evil is
-        ;; available
-        (add-hook 'srefactor-ui-menu-mode-hook 'evil-emacs-state)))
-    :config
-    (progn
-  (spacemacs/set-leader-keys-for-major-mode 'c-mode "r" 'srefactor-refactor-at-point)
-  (spacemacs/set-leader-keys-for-major-mode 'c++-mode "r" 'srefactor-refactor-at-point)
-    )
-  )
-)
+      (spacemacs/set-leader-keys-for-major-mode
+        'c-mode "r" 'srefactor-refactor-at-point)
+      (spacemacs/set-leader-keys-for-major-mode
+        'c++-mode "r" 'srefactor-refactor-at-point))))
 
 (defun ccpp/init-irony ()
   (use-package irony
     :defer t
-    :init
-    (progn
-      ;; setup irony-mode
-      (add-hook 'c++-mode-hook 'irony-mode)
-      (add-hook 'c-mode-hook 'irony-mode)
-      (add-hook 'objc-mode-hook 'irony-mode)
-      (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
-      )))
+    :init (progn
+            ;; setup irony-mode
+            (add-hook 'c++-mode-hook 'irony-mode)
+            (add-hook 'c-mode-hook 'irony-mode)
+            (add-hook 'objc-mode-hook 'irony-mode)
+            (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options))))
 
 (defun ccpp/init-irony-eldoc ()
   (use-package irony-eldoc
     :defer t
-    :init
-    (progn
-      (add-hook 'irony-mode-hook #'irony-eldoc))))
+    :init (progn
+            (add-hook 'irony-mode-hook #'irony-eldoc))))
 
 (defun ccpp/init-flycheck-irony ()
   (use-package flycheck-irony
     :defer t
-    :init
-    (progn
-      (with-eval-after-load 'flycheck
-        (require 'flycheck-irony)
-        (add-hook 'flycheck-mode-hook #'flycheck-irony-setup)
-        (flycheck-add-next-checker 'irony '(warning . c/c++-cppcheck))))))
+    :init (progn
+            (with-eval-after-load 'flycheck
+              (require 'flycheck-irony)
+              (add-hook 'flycheck-mode-hook #'flycheck-irony-setup)
+              (flycheck-add-next-checker 'irony
+                                         '(warning . c/c++-cppcheck))))))
 
 (defun ccpp/init-rtags ()
   (use-package rtags
     :defer t
-    :init
-    (progn
-  ;; setup rtags
-  (add-hook 'c-mode-hook 'rtags-start-process-unless-running)
-  (add-hook 'c++-mode-hook 'rtags-start-process-unless-running)
-  (add-hook 'objc-mode-hook 'rtags-start-process-unless-running)
-  (rtags-enable-standard-keybindings)
-  )))
+    :init (progn
+            ;; setup rtags
+            (add-hook 'c-mode-hook 'rtags-start-process-unless-running)
+            (add-hook 'c++-mode-hook 'rtags-start-process-unless-running)
+            (add-hook 'objc-mode-hook 'rtags-start-process-unless-running)
+            (rtags-enable-standard-keybindings))))
 
 (defun ccpp/init-helm-rtags ()
   (use-package helm-rtags
     :defer t
-    :init
-    (progn
-      (setq rtags-display-result-backend 'helm))))
+    :init (progn
+            (setq rtags-display-result-backend 'helm))))
 
 (defun ccpp/post-init-flycheck ()
   ;; enable flycheck for c-c++
   (add-hook 'c++-mode-hook 'flycheck-mode)
-  (add-hook 'c-mode-hook 'flycheck-mode)
-  )
+  (add-hook 'c-mode-hook 'flycheck-mode))
 
 (defun ccpp/init-cmake-mode ()
   (use-package cmake-mode
-    :mode (("CMakeLists\\.txt\\'" . cmake-mode) ("\\.cmake\\'" . cmake-mode))
-    :init (
-           ;;push 'company-cmake company-backends-cmake-mode
-                )))
+    :mode (("CMakeLists\\.txt\\'" . cmake-mode)
+           ("\\.cmake\\'" . cmake-mode)):init
+    (
+     ;;push 'company-cmake company-backends-cmake-mode
+     )))
 
 (defun ccpp/init-disaster ()
   (use-package disaster
     :defer t
-    :commands (disaster)
-    :init
+    :commands (disaster):init
     (progn
-      (spacemacs/set-leader-keys-for-major-mode 'c-mode
-        "D" 'disaster)
-      (spacemacs/set-leader-keys-for-major-mode 'c++-mode
-        "D" 'disaster))))
+      (spacemacs/set-leader-keys-for-major-mode
+        'c-mode "D" 'disaster)
+      (spacemacs/set-leader-keys-for-major-mode
+        'c++-mode "D" 'disaster))))
 
 (defun ccpp/init-clang-format ()
-  (use-package clang-format
-    :defer t))
+  (use-package clang-format :defer t))
+
+(defun ccpp/init-gdb-mi ()
+  (use-package gdb-mi
+    :defer t
+    :init (setq
+           ;; use gdb-many-windows by default when `M-x gdb'
+           gdb-many-windows t
+           ;; Non-nil means display source file containing the main routine at startup
+           gdb-show-main t)))
 
 ;;; packages.el ends here
