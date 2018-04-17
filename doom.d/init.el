199f14b98ed52e7a3c497b41cf87c3e52594fa4d
Felix M. Cobos <felix.cobos@gmail.com>
2018-04-17 20:49:27 +0200

    Issue #3 is now Issue #13

--- doom.d/init.el
+++ doom.d/init.el
@@ -33,7 +33,7 @@
        evil-goggles      ; display visual hints when editing in evil
        hl-todo           ; highlight TODO/FIXME/NOTE tags
        nav-flash         ; blink the current line after jumping
-      ;posframe          ; use child frames where possible (Emacs 26+ only)
+       posframe          ; use child frames where possible (Emacs 26+ only)
       ;tabbar            ; FIXME an (incomplete) tab bar for Emacs
       ;unicode           ; extended unicode support for various languages
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
@@ -48,11 +48,11 @@
        impatient-mode    ; show off code over HTTP
       ;macos             ; MacOS-specific commands
        make              ; run make tasks from Emacs
-      ;magit             ;
+       magit             ;
        neotree           ; a project drawer, like NERDTree for vim
        password-store    ; password manager for nerds
        pdf               ; pdf enhancements
-      ;rgb               ; creating color strings
+       rgb               ; creating color strings
        rotate-text       ; cycle region at point between text candidates
        term              ; terminals in Emacs
        tmux              ; an API for interacting with tmux
@@ -61,27 +61,27 @@
        :lang
        assembly          ; assembly for fun or debugging
        cc                ; C/C++/Obj-C madness
-       crystal           ; ruby at the speed of c
-       clojure           ; java with a lisp
-       csharp            ; unity, .NET, and mono shenanigans
+;       crystal           ; ruby at the speed of c
+;       clojure           ; java with a lisp
+;       csharp            ; unity, .NET, and mono shenanigans
        data              ; config/data formats
       ;erlang            ; an elegant language for a more civilized age
-       elixir            ; erlang done right
-       elm               ; care for a cup of TEA?
+;       elixir            ; erlang done right
+;       elm               ; care for a cup of TEA?
        emacs-lisp        ; drown in parentheses
-       ess               ; emacs speaks statistics
-       go                ; the hipster dialect
-       (haskell +intero) ; a language that's lazier than I am
-       hy                ; readability of scheme w/ speed of python
+;       ess               ; emacs speaks statistics
+;       go                ; the hipster dialect
+;       (haskell +intero) ; a language that's lazier than I am
+;       hy                ; readability of scheme w/ speed of python
        (java +meghanada) ; the poster child for carpal tunnel syndrome
        javascript        ; all(hope(abandon(ye(who(enter(here))))))
-       julia             ; a better, faster MATLAB
-       latex             ; writing papers in Emacs has never been so fun
-       ledger            ; an accounting system in Emacs
+;       julia             ; a better, faster MATLAB
+;       latex             ; writing papers in Emacs has never been so fun
+;       ledger            ; an accounting system in Emacs
        lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
-       nix               ; I hereby declare "nix geht mehr!"
-       ocaml             ; an objective camel
+;       nix               ; I hereby declare "nix geht mehr!"
+;       ocaml             ; an objective camel
        (org              ; organize your plain life in plain text
         +attach          ; custom attachment system
         +babel           ; running code in org
@@ -91,15 +91,15 @@
         +publish)        ; Emacs+Org as a static site generator
        perl              ; write code no one else can comprehend
        php               ; perl's insecure younger brother
-       plantuml          ; diagrams for confusing people more
+;       plantuml          ; diagrams for confusing people more
        purescript        ; javascript, but functional
        python            ; beautiful is better than ugly
        rest              ; Emacs as a REST client
-       ruby              ; 1.step do {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
+;       ruby              ; 1.step do {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
-       scala             ; java, but good
+;       scala             ; java, but good
        sh                ; she sells (ba|z)sh shells on the C xor
-       swift             ; who asked for emoji variables?
+;       swift             ; who asked for emoji variables?
        typescript        ; javascript, but better
        web               ; the tubes
 
@@ -131,7 +131,7 @@
        (setq doom-theme 'doom-vibrant)
        ;; Set line numbers style
        (setq doom-line-numbers-style 'relative)
-       ;; Disable blinking cursor
+       ;; Disable cursor blinking
        (blink-cursor-mode 0)
        (setq visible-cursor nil)
        ;; setup default indentation
@@ -140,4 +140,4 @@
        (setq default-tab-width 8)
        (setq c-basic-offset 8)
        (c-set-offset 'case-label '+)
- 
+
