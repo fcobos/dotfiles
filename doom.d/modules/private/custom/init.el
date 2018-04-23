74b33b89444490e1279b3ddae93f6c58479a9b92
Felix M. Cobos <felix.cobos@gmail.com>
2018-04-23 02:21:32 +0200

    Added a banner to the default admin page. Please have mercy on me =(

--- doom.d/modules/private/custom/init.el
+++ doom.d/modules/private/custom/init.el
@@ -1,3 +1,7 @@
 ;; Disable cursor blink
 (add-hook 'doom-post-init-hook (lambda () (blink-cursor-mode 0)))
 (setq visible-cursor nil)
+;; Disable whitespace-mode
+(add-hook 'doom-post-init-hook (lambda () 
+  (remove-hook 'editorconfig-custom-hooks 'doom|editorconfig-whitespace-mode-maybe)))
+
