c225918c04806c2b8a6f8804078114533b900d04
Felix M. Cobos <felix.cobos@gmail.com>
2018-05-08 16:15:45 +0200

    it is hump day _^_

--- doom.d/modules/private/custom/init.el
+++ doom.d/modules/private/custom/init.el
@@ -2,6 +2,28 @@
 (add-hook 'doom-post-init-hook (lambda () (blink-cursor-mode 0)))
 (setq visible-cursor nil)
 ;; Disable whitespace-mode
-(add-hook 'doom-post-init-hook (lambda () 
+(add-hook 'doom-post-init-hook (lambda ()
   (remove-hook 'editorconfig-custom-hooks 'doom|editorconfig-whitespace-mode-maybe)))
+;; Change dashboard banner
+(defun custom-dashboard-widget-banner ()
+  (mapc (lambda (line)
+          (insert (propertize (+doom-dashboard--center +doom-dashboard--width line)
+                              'face 'font-lock-comment-face) " ")
+          (insert "\n"))
+        '("                                           "
+          "███████╗███╗   ███╗ █████╗  ██████╗███████╗"
+          "██╔════╝████╗ ████║██╔══██╗██╔════╝██╔════╝"
+          "█████╗  ██╔████╔██║███████║██║     ███████╗"
+          "██╔══╝  ██║╚██╔╝██║██╔══██║██║     ╚════██║"
+          "███████╗██║ ╚═╝ ██║██║  ██║╚██████╗███████║"
+          "╚══════╝╚═╝     ╚═╝╚═╝  ╚═╝ ╚═════╝╚══════╝"
+          "                                           ")))
+
+(defvar +doom-dashboard-functions '(custom-dashboard-widget-banner
+                                    doom-dashboard-widget-shortmenu
+                                    doom-dashboard-widget-loaded)
+  "List of widget functions to run in the dashboard buffer to construct the
+dashboard. These functions take no arguments and the dashboard buffer is current
+while they run.")
+
 
