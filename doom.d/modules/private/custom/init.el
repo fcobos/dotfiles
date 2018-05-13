1f48383cf35ea08d1f22297e8af1dd16ee15dcef
Felix M. Cobos <felix.cobos@gmail.com>
2018-05-14 00:51:48 +0200

    yo recipes

--- doom.d/modules/private/custom/init.el
+++ doom.d/modules/private/custom/init.el
@@ -52,3 +52,7 @@ while they run.")
 ;; Enable gdb many windows.
 (setq gdb-many-windows t)
 
+;; qml-mode
+(autoload 'qml-mode "qml-mode" "Editing Qt Declarative." t)
+(add-to-list 'auto-mode-alist '("\\.qml$" . qml-mode))
+
