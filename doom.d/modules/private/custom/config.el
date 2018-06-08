3b42a7b2b0230512f9f736e2a85d036dfb9c823c
Felix M. Cobos <felix.cobos@gmail.com>
2018-06-08 02:39:05 +0200

    some brief changes

--- doom.d/modules/private/custom/config.el
+++ doom.d/modules/private/custom/config.el
@@ -2,7 +2,9 @@
 (add-hook! 'after-init-hook
   (map!
     (:leader
-      :desc "M-x" :n "SPC" #'execute-extended-command)))
+      :desc "M-x" :n "SPC" #'execute-extended-command
+      (:desc "code" :prefix "c"
+        :desc "Format buffer" :n "f" #'format-all-buffer))))
 
 ;; pipenv config
 (use-package pipenv
