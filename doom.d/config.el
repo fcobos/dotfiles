3363cf827342a7a1a9c7c18c79058fbe1462fa9b
Felix M. Cobos <felix.cobos@gmail.com>
2018-06-09 03:42:36 +0200

    TODO: write meaningful commit message

--- doom.d/config.el
+++ doom.d/config.el
@@ -1,3 +1,16 @@
+;; Custom keybindings
+(add-hook! 'after-init-hook
+  (map!
+    (:leader
+      :desc "M-x" :n "SPC" #'execute-extended-command
+      (:desc "code" :prefix "c"
+        :desc "Format buffer" :n "f" #'format-all-buffer))))
+
+;; pipenv config
+(use-package pipenv
+  :hook (python-mode . pipenv-mode))
+
+
 ;; Maximize frame
 ;; (add-hook 'doom-init-hook (lambda () (toggle-frame-maximized)))
 
