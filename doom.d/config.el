45f487ad49f2d73fcb9ffc44715e3c945dd5aee7
Felix M. Cobos <felix.cobos@gmail.com>
2018-06-12 23:14:23 +0200

    fail

--- doom.d/config.el
+++ doom.d/config.el
@@ -87,3 +87,7 @@
 
 ;; better scrolling performance maybe...
 (setq auto-window-vscroll nil)
+(setq scroll-margin 1
+      scroll-conservatively 0
+      scroll-up-aggressively 0.01
+      scroll-down-aggressively 0.01)
