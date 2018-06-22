ee68a5beb1bcf72bdf1082250329c43998965153
Felix M. Cobos <felix.cobos@gmail.com>
2018-06-23 01:57:04 +0200

    It compiles! Ship it!

--- doom.d/config.el
+++ doom.d/config.el
@@ -10,7 +10,7 @@
 (setq nsm-save-host-names t)
 (setq tls-program
        '("gnutls-cli -p %p --dh-bits=2048 --x509cafile=%t \
-            --priority='SECURE192:+SECURE128:-VERS-ALL:+VERS-TLS1.2' %h"))
+            --priority='SECURE192:+SECURE128:-VERS-ALL:+VERS-TLS1.2:%PROFILE_MEDIUM' %h"))
 (setq tls-checktrust t)
 
 ;; Custom keybindings
