d59dad6bdb8edf9e859b0ffeb1cddd0f74c776dd
Felix M. Cobos <felix.cobos@gmail.com>
2018-08-01 16:23:34 +0200

    Spinning up the hamster...

--- doom.d/config.el
+++ doom.d/config.el
@@ -208,4 +208,9 @@
 (setq eshell-buffer-maximum-lines 1000)
 (add-hook 'eshell-output-filter-functions #'eshell-truncate-buffer)
 
+;; imenu on the left side
+(after! imenu-list
+  (set-popup-rule! "^\\*Ilist"
+    :side 'left :size 35 :quit nil :select nil :ttl 0))
+
 ;;; config.el ends here
