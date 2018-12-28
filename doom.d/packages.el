8c0df6416db38b4b2ce973ab611a66f97b87f64e
Felix M. Cobos <felix.cobos@gmail.com>
2018-12-28 15:08:50 +0100

    workaround evil-matchit bug

--- doom.d/packages.el
+++ doom.d/packages.el
@@ -3,3 +3,5 @@
 (package! traad)
 ;; pkgbuild-mode
 (package! pkgbuild-mode)
+;; workaround evil-matchit bug
+(package! evil-matchit :recipe (:fetcher github :repo "redguardtoo/evil-matchit" :commit "7d65b4167b1f0086c2b42b3aec805e47a0d355c4"))
