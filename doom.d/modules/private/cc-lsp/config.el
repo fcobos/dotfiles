4a4000aa3dad80a3c6cf673781de90b95c162f5f
Felix M. Cobos <felix.cobos@gmail.com>
2019-02-18 21:22:03 +0100

    ccls configuration

--- doom.d/modules/private/cc-lsp/config.el
+++ doom.d/modules/private/cc-lsp/config.el
@@ -11,23 +11,7 @@
   :defer t
   :hook ((c-mode c++-mode cuda-mode objc-mode) . (lambda () (require 'ccls) (lsp)))
   :init
-  ;;(setq ccls-initialization-options '(:index (:comments 2) :completion (:detailedLabel t)))
-  (setq ccls-initialization-options
-   '(:clang
-     (:excludeArgs
-      ;; Linux's gcc options. See ccls/wiki
-      ["-falign-jumps=1" "-falign-loops=1" "-fconserve-stack" "-fmerge-constants" "-fno-code-hoisting" "-fno-schedule-insns" "-fno-var-tracking-assignments" "-fsched-pressure"
-       "-mhard-float" "-mindirect-branch-register" "-mindirect-branch=thunk-inline" "-mpreferred-stack-boundary=2" "-mpreferred-stack-boundary=3" "-mpreferred-stack-boundary=4" "-mrecord-mcount" "-mindirect-branch=thunk-extern" "-mno-fp-ret-in-387" "-mskip-rax-setup"
-       "--param=allow-store-data-races=0" "-Wa arch/x86/kernel/macros.s" "-Wa -"]
-      :extraArgs ["--gcc-toolchain=/usr"]
-      :pathMappings ,+ccls-path-mappings)
-     :completion
-     (:include
-      (:blacklist
-       ["^/usr/(local/)?include/c\\+\\+/[0-9\\.]+/(bits|tr1|tr2|profile|ext|debug)/"
-        "^/usr/(local/)?include/c\\+\\+/v1/"
-        ]))
-     :index (:initialBlacklist ,+ccls-initial-blacklist :trackDependency 1)))
+  (setq ccls-initialization-options '(:index (:comments 2) :completion (:detailedLabel t)))
   :config
   (setq ccls-sem-highlight-method 'font-lock)
   (add-to-list 'projectile-globally-ignored-directories ".ccls-cache")
