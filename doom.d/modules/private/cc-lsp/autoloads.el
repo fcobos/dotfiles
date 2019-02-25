c95baa192f3d8a0df71cd7571cdc7cfbec2838a4
Felix M. Cobos <felix.cobos@gmail.com>
2019-02-26 00:33:26 +0100

    Replace Doom's cc with our own.

--- doom.d/modules/private/cc-lsp/autoloads.el
+++
@@ -1,7 +0,0 @@
-;;; private/cc-lsp/autoloads.el -*- lexical-binding: t; -*-
-
-;;;###autoload
-(defvar +ccls-path-mappings [])
-
-;;;###autoload
-(defvar +ccls-initial-blacklist [])
--- doom.d/modules/private/cc-lsp/packages.el
+++ doom.d/modules/private/cc-lsp/packages.el
@@ -4,3 +4,15 @@
 (package! lsp-mode)
 ;;(package! cquery)
 (package! ccls)
+
+;; extra stuff
+(package! cmake-mode)
+(package! cuda-mode)
+(package! demangle-mode)
+(package! disaster)
+(package! modern-cpp-font-lock)
+(package! opencl-mode)
+
+(when (package! glsl-mode)
+  (when (featurep! :completion company)
+    (package! company-glsl :recipe (:fetcher github :repo "Kaali/company-glsl"))))
