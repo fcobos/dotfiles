81f77202bcde1a75b73a345aea88fdceba294e27
Felix M. Cobos <felix.cobos@gmail.com>
2019-03-09 03:28:10 +0100

    Formatting.

--- doom.d/init.el
+++ doom.d/init.el
@@ -119,7 +119,7 @@
        ;;plantuml          ; diagrams for confusing people more
        purescript        ; javascript, but functional
        (python           ; beautiful is better than ugly
-	 +lsp
+        +lsp
         +pyenv)
        qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
