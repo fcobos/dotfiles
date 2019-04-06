c642c7c1f37d46dd82340740e01e0e2a50de6065
Felix M. Cobos <felix.cobos@gmail.com>
2019-04-06 08:16:37 +0200

    Leader "c" workaround.

--- doom.d/config.el
+++ doom.d/config.el
@@ -111,4 +111,21 @@
   (add-hook 'after-init-hook (lambda ()
                                (toggle-frame-maximized))))
 
+;; c key mapping workaround
+(add-hook
+ 'after-change-major-mode-hook
+ (lambda ()
+   (map! :leader
+         (:prefix ("c" . "code")
+           :desc "Compile project"             "c"   #'projectile-compile-project
+           :desc "Jump to definition"          "d"   #'+lookup/definition
+           :desc "Jump to references"          "D"   #'+lookup/references
+           :desc "Evaluate buffer/region"      "e"   #'+eval/buffer-or-region
+           :desc "Evaluate & replace region"   "E"   #'+eval:replace-region
+           :desc "Format buffer/region"        "f"   #'+format/region-or-buffer
+           :desc "Open REPL"                   "r"   #'+eval/open-repl-other-window
+           :desc "Delete trailing whitespace"  "w"   #'delete-trailing-whitespace
+           :desc "Delete trailing newlines"    "W"   #'doom/delete-trailing-newlines
+           :desc "List errors"                 "x"   #'flycheck-list-errors))))
+
 ;;; config.el ends here
