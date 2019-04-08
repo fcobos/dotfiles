9ce056479b98557121ee76a76ae8e01c2cca7bde
Felix M. Cobos <felix.cobos@gmail.com>
2019-04-08 20:13:54 +0200

    Sort

--- scripts/go-tools.sh
+++ scripts/go-tools.sh
@@ -1,15 +1,11 @@
 #!/usr/bin/env bash
 
-go get github.com/rogpeppe/godef
-go get -u github.com/motemen/gore
-#go get -u github.com/mdempsky/gocode
-#go get -u github.com/stamblerre/gocode
-go get -u github.com/visualfc/gocode
 go get -u golang.org/x/tools/cmd/godoc
-go get -u github.com/zmb3/gogetdoc
 go get -u golang.org/x/tools/cmd/goimports
 go get -u golang.org/x/tools/cmd/gorename
 go get -u golang.org/x/tools/cmd/guru
+go get -u golang.org/x/lint/golint
+go get -u golang.org/x/mobile/cmd/gomobile
 #go get -u golang.org/x/tools/cmd/gopls
 #mkdir ~/.cache/go-tools
 #cd ~/.cache/go-tools
@@ -18,14 +14,17 @@ go get -u golang.org/x/tools/cmd/guru
 #go install
 #cd ~/
 #rm -rf ~/.cache/go-tools
-
 go get -u github.com/saibing/bingo
 
+go get -u github.com/rogpeppe/godef
+go get -u github.com/motemen/gore
+#go get -u github.com/mdempsky/gocode
+#go get -u github.com/stamblerre/gocode
+go get -u github.com/visualfc/gocode
+go get -u github.com/zmb3/gogetdoc
 go get -u github.com/elliotchance/c2go
 go get -u github.com/go-delve/delve/cmd/dlv
-go get -u golang.org/x/lint/golint
 go get -u github.com/jstemmer/gotags
-
 go get -u github.com/klauspost/asmfmt/cmd/asmfmt
 go get -u github.com/kisielk/errcheck
 go get -u github.com/davidrjenni/reftools/cmd/fillstruct
@@ -36,8 +35,6 @@ go get -u github.com/josharian/impl
 go get -u honnef.co/go/tools/cmd/keyify
 go get -u github.com/fatih/motion
 go get -u github.com/koron/iferr
-
-go get -u golang.org/x/mobile/cmd/gomobile
 
 # strip binaries
 cd $GOPATH/bin
