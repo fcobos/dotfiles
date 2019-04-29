58cfa197fd2ec3f6a8908ffb4863d91e50061949
Felix M. Cobos <felix.cobos@gmail.com>
2019-04-29 16:30:09 +0200

    Revert "Remove tools not needed."
    This reverts commit 626a18ccd422d6eb2a4cf55970d93afe1005f847.

--- scripts/go-tools.sh
+++ scripts/go-tools.sh
@@ -15,7 +15,7 @@ go get -u golang.org/x/tools/cmd/gopls
 #go install
 #cd ~/
 #rm -rf ~/.cache/go-tools
-#go get -u github.com/saibing/bingo
+go get -u github.com/saibing/bingo
 
 go get -u github.com/rogpeppe/godef
 go get -u github.com/motemen/gore
@@ -23,15 +23,22 @@ go get -u github.com/motemen/gore
 #go get -u github.com/stamblerre/gocode
 go get -u github.com/visualfc/gocode
 go get -u github.com/zmb3/gogetdoc
+go get -u github.com/elliotchance/c2go
 go get -u github.com/go-delve/delve/cmd/dlv
-#go get -u github.com/jstemmer/gotags
-#go get -u github.com/kisielk/errcheck
+go get -u github.com/jstemmer/gotags
+go get -u github.com/klauspost/asmfmt/cmd/asmfmt
+go get -u github.com/kisielk/errcheck
+go get -u github.com/davidrjenni/reftools/cmd/fillstruct
+go get -u github.com/alecthomas/gometalinter
 go get -u github.com/golangci/golangci-lint/cmd/golangci-lint
 go get -u github.com/fatih/gomodifytags
 go get -u github.com/josharian/impl
 go get -u honnef.co/go/tools/cmd/keyify
+go get -u github.com/fatih/motion
+go get -u github.com/koron/iferr
 go get -u github.com/mdempsky/unconvert
 go get -u honnef.co/go/tools/cmd/staticcheck
+go get -u github.com/kisielk/errcheck
 
 # strip binaries
 cd $GOPATH/bin
