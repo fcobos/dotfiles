330d6c630a39d1060a868e93e5dbfbd2aafded86
Felix M. Cobos <felix.cobos@gmail.com>
2019-05-02 21:27:49 +0200

    Add ldflags

--- scripts/go-tools.sh
+++ scripts/go-tools.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 cd ~/
 
-go get -u golang.org/x/tools/cmd/godoc
-go get -u golang.org/x/tools/cmd/goimports
-go get -u golang.org/x/tools/cmd/gorename
-go get -u golang.org/x/tools/cmd/guru
-go get -u golang.org/x/lint/golint
-go get -u golang.org/x/mobile/cmd/gomobile
-go get -u golang.org/x/tools/cmd/gopls
+go get -u -ldflags "-s -w" golang.org/x/tools/cmd/godoc
+go get -u -ldflags "-s -w" golang.org/x/tools/cmd/goimports
+go get -u -ldflags "-s -w" golang.org/x/tools/cmd/gorename
+go get -u -ldflags "-s -w" golang.org/x/tools/cmd/guru
+go get -u -ldflags "-s -w" golang.org/x/lint/golint
+go get -u -ldflags "-s -w" golang.org/x/mobile/cmd/gomobile
+go get -u -ldflags "-s -w" golang.org/x/tools/cmd/gopls
 #mkdir ~/.cache/go-tools
 #cd ~/.cache/go-tools
 #git clone -b bingo https://github.com/saibing/tools.git
@@ -15,32 +15,28 @@ go get -u golang.org/x/tools/cmd/gopls
 #go install
 #cd ~/
 #rm -rf ~/.cache/go-tools
-#go get -u github.com/saibing/bingo
+#go get -u -ldflags "-s -w" github.com/saibing/bingo
 
-go get -u github.com/rogpeppe/godef
-go get -u github.com/motemen/gore
-#go get -u github.com/mdempsky/gocode
-#go get -u github.com/stamblerre/gocode
-go get -u github.com/visualfc/gocode
-go get -u github.com/zmb3/gogetdoc
-go get -u github.com/elliotchance/c2go
-go get -u github.com/go-delve/delve/cmd/dlv
-go get -u github.com/jstemmer/gotags
-go get -u github.com/klauspost/asmfmt/cmd/asmfmt
-go get -u github.com/davidrjenni/reftools/cmd/fillstruct
-go get -u github.com/alecthomas/gometalinter
-go get -u github.com/golangci/golangci-lint/cmd/golangci-lint
-go get -u github.com/fatih/gomodifytags
-go get -u github.com/josharian/impl
-go get -u honnef.co/go/tools/cmd/keyify
-go get -u github.com/fatih/motion
-go get -u github.com/koron/iferr
-go get -u github.com/mdempsky/unconvert
-go get -u honnef.co/go/tools/cmd/staticcheck
-#go get -u github.com/kisielk/errcheck
-go get -u github.com/godoctor/godoctor
-go get -u github.com/cweill/gotests/...
-
-# strip binaries
-cd $GOPATH/bin
-for f in * ; do strip --strip-all $f ; done
+go get -u -ldflags "-s -w" github.com/rogpeppe/godef
+go get -u -ldflags "-s -w" github.com/motemen/gore
+#go get -u -ldflags "-s -w" github.com/mdempsky/gocode
+#go get -u -ldflags "-s -w" github.com/stamblerre/gocode
+go get -u -ldflags "-s -w" github.com/visualfc/gocode
+go get -u -ldflags "-s -w" github.com/zmb3/gogetdoc
+go get -u -ldflags "-s -w" github.com/elliotchance/c2go
+go get -u -ldflags "-s -w" github.com/go-delve/delve/cmd/dlv
+go get -u -ldflags "-s -w" github.com/jstemmer/gotags
+go get -u -ldflags "-s -w" github.com/klauspost/asmfmt/cmd/asmfmt
+go get -u -ldflags "-s -w" github.com/davidrjenni/reftools/cmd/fillstruct
+go get -u -ldflags "-s -w" github.com/alecthomas/gometalinter
+go get -u -ldflags "-s -w" github.com/golangci/golangci-lint/cmd/golangci-lint
+go get -u -ldflags "-s -w" github.com/fatih/gomodifytags
+go get -u -ldflags "-s -w" github.com/josharian/impl
+go get -u -ldflags "-s -w" honnef.co/go/tools/cmd/keyify
+go get -u -ldflags "-s -w" github.com/fatih/motion
+go get -u -ldflags "-s -w" github.com/koron/iferr
+go get -u -ldflags "-s -w" github.com/mdempsky/unconvert
+go get -u -ldflags "-s -w" honnef.co/go/tools/cmd/staticcheck
+go get -u -ldflags "-s -w" github.com/kisielk/errcheck
+go get -u -ldflags "-s -w" github.com/godoctor/godoctor
+go get -u -ldflags "-s -w" github.com/cweill/gotests/...
