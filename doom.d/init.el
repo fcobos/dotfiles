d465861bb3c64e5d6b789b8407d4976d814f2119
Felix M. Cobos <felix.cobos@gmail.com>
2019-09-17 09:25:57 +0200

    Enable all ivy options.

--- doom.d/init.el
+++ doom.d/init.el
@@ -16,7 +16,10 @@
        company           ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       ivy               ; a search engine for love and life
+       (ivy +fuzzy
+	    +icons
+	    +prescient
+	    +childframe)               ; a search engine for love and life
 
        :ui
        ;;deft              ; notational velocity for Emacs
