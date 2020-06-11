3f78fc7b7e813f027b9f0067f825f9bfd37bd7b9
Felix M. Cobos <felix.cobos@gmail.com>
2020-06-11 22:03:58 +0200

    Remove custom modules.

--- doom.d/modules/private/go-custom/packages.el
+++
@@ -1,8 +0,0 @@
-;; -*- no-byte-compile: t; -*-
-;;; private/go-custom/packages.el
-
-(package! go-tag)
-(package! go-fill-struct)
-(package! go-gen-test)
-(package! godoctor)
-(package! go-impl)
--- doom.d/packages.el
+++
@@ -1,9 +0,0 @@
-;; -*- lexical-binding: t; no-byte-compile: t; -*-
-;; emacs-traad for python refactoring
-(package! traad)
-;; pkgbuild-mode
-(package! pkgbuild-mode)
-;; vimrc-mode
-(package! vimrc-mode)
-;; anaconda-mode seems to require posframe
-(package! posframe)
