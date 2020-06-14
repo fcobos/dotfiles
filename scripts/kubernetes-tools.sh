55c0dd5683ff6abb88c10ccb2991a03cd50cb4dd
Felix M. Cobos <felix.cobos@gmail.com>
2020-06-14 12:04:28 +0200

    Add kubectl

--- scripts/kubernetes-tools.sh
+++ scripts/kubernetes-tools.sh
@@ -7,10 +7,15 @@ chmod 700 get_helm.sh
 ./get_helm.sh
 rm -f ./get_helm.sh
 
+# kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv kubectl ~/bin/
+
 # minikube
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 mv minikube-linux-amd64 ~/bin/minikube
-chmod 755 ~/bin/minikube
+chmod +x ~/bin/minikube
 
 # minishift
 VERSION=$(curl -s https://github.com/minishift/minishift/releases/latest/download 2>&1 | grep -Po "[0-9]+\.[0-9]+\.[0-9]+")
