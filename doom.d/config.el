7498460fe43ad1d25b405f59d7afb12964e8fdf8
Felix M. Cobos <felix.cobos@posteo.net>
2020-10-04 14:21:50 +0200

    Truncate buffer name.

--- doom.d/config.el
+++ doom.d/config.el
@@ -17,7 +17,7 @@
 (setq doom-theme 'doom-one-light)
 
 ;; configure mode-line
-(setq doom-modeline-buffer-file-name-style 'truncate-upto-root)
+(setq doom-modeline-buffer-file-name-style 'truncate-all)
 
 ;; faces configuration
 (set-face-attribute 'default nil :height 105 :weight 'normal)
