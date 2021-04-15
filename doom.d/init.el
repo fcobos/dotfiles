49ffdc59ddd07ccb9150917430783c5587c73a86
Felix M. Cobos <felix.cobos@posteo.net>
2021-04-15 14:27:59 +0200

    Update init.el

--- doom.d/init.el
+++ doom.d/init.el
@@ -168,8 +168,8 @@
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
-       ;;web               ; the tubes
-       ;;yaml              ; JSON, but readable
+       (web +html +css)  ; the tubes
+       yaml              ; JSON, but readable
        ;;zig               ; C, but simpler
 
        :email
