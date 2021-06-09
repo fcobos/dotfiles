bb0fad8fb2d33ed0da1df12168ee65a7cfb552f9
Felix M. Cobos <felix.cobos@posteo.net>
2021-06-09 08:37:16 +0200

    change theme on system appearance change

--- doom.d/config.el
+++ doom.d/config.el
@@ -47,6 +47,16 @@
             (insert "\n"))
       dashboard-banner-list)))
 
+;; macOS - change theme on system appearance change
+(defun my/apply-theme (appearance)
+  "Load theme, taking current system APPEARANCE into consideration."
+  (mapc #'disable-theme custom-enabled-themes)
+  (pcase appearance
+    ('light (load-theme 'doom-one-light t))
+    ('dark (load-theme 'doom-one t))))
+
+(add-hook 'ns-system-appearance-change-functions #'my/apply-theme)
+
 ;; Enable gdb many windows.
 (setq gdb-many-windows t)
 ;; python debugger
