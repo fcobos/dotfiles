960e4e9549b868f68f67873e526f35314879932c
Felix M. Cobos <felix.cobos@posteo.net>
2023-07-02 08:39:25 +0200

    Auto low power mode fixes.

--- scripts/macos-auto-low-power.sh
+++ scripts/macos-auto-low-power.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env sh
 
-while [ 0 == 0 ]
+while true
 do
+	power_source=$(pmset -g batt | head -n 1 | cut -d \' -f2)
 	batt_pct=$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)
 
-	if [ $batt_pct -lt 25 ]; then
+	if [ "$batt_pct" -lt 25 ] && [ "$power_source" = "Battery Power" ]; then
 		pmset -a lowpowermode 1
 	fi
 
-	if [ $batt_pct -gt 50 ]; then
+	if [ "$batt_pct" -gt 25 ] && [ "$power_source" = "AC Power" ]; then
 		pmset -a lowpowermode 0
 	fi
 
