eeb0208fd91fbd8321d192c96018dadf946a1e11
Felix M. Cobos <felix.cobos@posteo.net>
2023-11-05 01:05:49 +0100

    BBC Basic mode.

--- doom.d/packages.el
+++ doom.d/packages.el
@@ -1,3 +1,4 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ../dotfiles/doom.d/packages.el
-(package! basic-mode)
+(package! basic-mode
+  :recipe (:host github :repo "fcobos/basic-mode" :files ("*.el" "src/*.el")))
