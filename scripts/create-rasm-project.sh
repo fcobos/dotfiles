a8d0853cee225a72f4e78ffdaadb71348221aee3
Felix M. Cobos <felix.cobos@posteo.net>
2023-12-10 12:07:53 +0100

    export symbols

--- scripts/create-rasm-project.sh
+++ scripts/create-rasm-project.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
-proj_name=$@
+proj_name=$1
 
-cp -r ~/dotfiles/scripts/rasm_project_template ./$proj_name
+cp -r ~/dotfiles/scripts/rasm_project_template ./"$proj_name"
 (
-    cd $proj_name || exit
+    cd "$proj_name" || exit
     find . -type f -not -path '*/\.*' -print0 | xargs -0 sed -i '' -e "s/PROJECT_NAME/$proj_name/g"
 )
--- scripts/rasm_project_template/build.sh
+++ scripts/rasm_project_template/build.sh
@@ -2,4 +2,4 @@
 rm -rf build
 mkdir build
 
-rasm src/main.asm
+rasm -eo -xr -utf8 -rasm -os build/PROJECT_NAME.sym src/main.asm
