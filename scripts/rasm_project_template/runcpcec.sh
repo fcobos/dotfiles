3f33eaeb43ccef14652171380de1ffd038c7d8dd
Felix M. Cobos <felix.cobos@posteo.net>
2025-11-11 00:34:38 +0100

    Update rasm project template

---
+++ scripts/rasm_project_template/runcpcec.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+~/software/cpcec/cpcec -g3 -k4 -m3 -c7 build/PROJECT_NAME.dsk > /dev/null 2>&1 &
--- scripts/rasm_project_template/runrvm.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-~/Applications/Retro\ Virtual\ Machine\ 2.app/Contents/MacOS/Retro\ Virtual\ Machine\ 2 -b=cpc6128 -i build/PROJECT_NAME.dsk -c='run"PROJECT_NAME\n' > /dev/null 2>&1 &
